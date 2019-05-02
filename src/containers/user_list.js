e016c33b221079f8010adbcbca7a3fd81da584ea
Sebastien Jarjanette <sjarj@pc27.home>
2019-05-02 21:43:34 +0200

    Add action selectUser

--- src/containers/user_list.js
+++ src/containers/user_list.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectUser } from '../actions';
+import { bindActionCreators } from 'redux';
 
 class UserList extends Component {
   render() {
-    console.log(this.props.users);
     return (
       <div>
         <ul className="col-md-4">
           {this.props.users.map(user => {
             return (
-              <li className="list-group-item" key={user.id}>
+              <li
+                className="list-group-item"
+                key={user.id}
+                onClick={() => this.props.selectUser(user)}>
                 {user.name}
               </li>
             );
@@ -20,8 +24,15 @@ class UserList extends Component {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return bindActionCreators({ selectUser: selectUser }, dispatch);
+};
+
 const mapStateToProps = state => {
   return { users: state.users };
 };
 
-export default connect(mapStateToProps)(UserList);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UserList);
